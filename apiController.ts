dd18b560ee12b0ec1eda871334075a52b3740abd
rlaalsrl <rlaalsrl0810@naver.com>
2019-09-19 21:30:45 +0900

    apiController update

--- apiController.ts
+++ apiController.ts
@@ -2,4 +2,12 @@ import { Router } from 'express';
 
 const router = Router();
 
+
+import userController from './routes/controllers/user.controller';
+import boardController from './routes/controllers/board.controller';
+import commentController from './routes/controllers/comment.controller';
+
+router.use('/user', userController);
+router.use('/board', boardController);
+
 export default router;
