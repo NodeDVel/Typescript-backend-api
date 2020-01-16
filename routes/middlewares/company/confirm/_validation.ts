90efe4e2381fabdef66840d6782b98f0ee038e49
rlalasrl <rlaalsrl0810@naver.com>
2020-01-16 16:40:20 +0900

    company_pk validator confirm

---
+++ routes/middlewares/company/confirm/_validation.ts
@@ -0,0 +1,16 @@
+import { body, ValidationChain } from 'express-validator';
+
+import CompanyRecruit from '@Model/companyRecurit';
+
+// tslint:disable-next-line: interface-name
+export interface PostCompanyConfirmRequest {
+  body: {
+    company_pk: CompanyRecruit['pk'],
+  },
+}
+
+const PostCompanyConfirmValidation: ValidationChain[] = [
+  body('company_pk').isInt(),
+]
+
+export default PostCompanyConfirmValidation;
