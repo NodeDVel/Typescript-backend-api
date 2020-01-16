7a3dc8e9f528c16b67ef1d936252bcea8873cf24
rlalasrl <rlaalsrl0810@naver.com>
2020-01-16 15:57:36 +0900

    Eliminate redundant support

---
+++ routes/middlewares/company/count/personnel/getCompanyPersonnelCount.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from 'express';
+import * as _ from 'lodash';
+
+import CustomError from '@Middleware/error/customError';
+
+import CompanyRecruit from '@Model/companyRecurit';
+import User from '@Model/user.model';
+
+
+const getCompanyPersonnelCount = async (req: Request, res: Response, next: NextFunction) => {
+  const user: User = res.locals.user;
+  const company_pk: CompanyRecruit['pk'] = req.query;
+
+  try {
+    const companyPersonnels: CompanyRecruit[] = await CompanyRecruit.findOne({ where: { pk: company_pk, user_pk: user.pk, confirm: true }});
+
+    const uniqPersonnels: CompanyRecruit[] = _.uniqBy(companyPersonnels, personnel => personnel.user_pk);
+
+    res.json({
+      success: true,
+      data: {
+        companyPersonnelCount: uniqPersonnels.length,
+      },
+    });
+    
+  } catch(error) {
+    console.log(error);
+    next(new CustomError({ name: 'Database_Error' }));
+  }
+}
+
+export default getCompanyPersonnelCount; // 신청 인원 반환 및 중복 제거
