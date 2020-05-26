c38133dd34bd95458bbefda0f30a4da10f5ac2aa
rlalasrl <rlaalsrl0810@naver.com>
2020-05-26 09:08:57 +0900

    /POST group

---
+++ routes/middlewares/group/post/postGroup.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response} from 'express';
+
+import User from '@Model/user.model';
+
+import CustomError from '@Middleware/error/customError';
+import Group from '@Model/group.model';
+
+const postGroup = async (req: Request, res: Response, next: NextFunction) => {
+  const user: User = res.locals.user;
+  const groupName: string= req.body;
+  const groupInformation: string | undefined = req.body;
+
+  try {
+    if(!user){
+      next(new CustomError({ name: 'Wrong_Data' }));
+    } else {
+      const group: Group = await Group.create({
+        user_pk: user.pk,
+        groupName,
+        groupInformation,
+      });
+
+      res.json({
+        success: true,
+        data: {
+          group: {
+            pk: group.pk,
+            groupName: group.groupName,
+            groupInformation: group.groupInformation,
+          },
+        },
+      });
+    }
+
+  } catch(err) {
+    console.log(err);
+    next(new CustomError({ name: 'Database_Error' }))
+  }
+}
+
+export default postGroup;
