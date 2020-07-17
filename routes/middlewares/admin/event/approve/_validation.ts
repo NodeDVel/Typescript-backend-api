89bf4213de24abcfe3a6ab92d96f52377b757e01
rlalasrl <rlaalsrl0810@naver.com>
2020-07-17 10:04:10 +0900

    controllers

--- routes/middlewares/admin/event/approve/_validation.ts
+++ routes/middlewares/admin/event/approve/_validation.ts
@@ -3,7 +3,6 @@ import { body, ValidationChain } from 'express-validator';
 import Event from '@Model/event.model';
 
 // tslint:disable-next-line: interface-name
-// tslint:disable-next-line: class-name
 export interface approveEventRequest {
   body: {
     event_pk: Event['pk'];
